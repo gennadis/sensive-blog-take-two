1ff585ac9680fe5111f9231f6ceed31c0a279816
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-17 09:50:09 +0300

    Apply code linting to `urls`

--- sensive_blog/urls.py
+++ sensive_blog/urls.py
@@ -6,11 +6,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('page/<int:page>', views.index, name='index'),
-    path('post/<slug:slug>', views.post_detail, name='post_detail'),
-    path('tag/<slug:tag_title>', views.tag_filter, name='tag_filter'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('', views.index, name='index'),
+    path("admin/", admin.site.urls),
+    path("page/<int:page>", views.index, name="index"),
+    path("post/<slug:slug>", views.post_detail, name="post_detail"),
+    path("tag/<slug:tag_title>", views.tag_filter, name="tag_filter"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("", views.index, name="index"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
